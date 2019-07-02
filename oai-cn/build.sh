b3b18e22e9f43f441fec77093e15b6836dc7ac7e
kevin Xu <tig4605246@gmail.com>
2019-07-02 10:01:22 +0200

    Rebuild the  repo

--- oai-cn/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker container rm oaicn -f
-docker rmi tig4605246/oaicn:1.1
-cp ~/kubernetes/go/src/snap-hook-for-docker/cmd/hook/hook ./
-docker build -t tig4605246/oaicn:1.1 --force-rm=true --rm=true .
--- oai-ran/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker container prune -f
-docker rmi tig4605246/oairan:1.0
-cp ~/kubernetes/go/src/snap-hook-for-docker/cmd/hook/hook ./
-docker build -t tig4605246/oairan:1.0 --force-rm=true --rm=true .
